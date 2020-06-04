5103b4d9751f19eedad416a69e99e3e6bd5e9ed7
Amilton Soares <app@amiltonsoares.com.br>
2020-06-04 13:47:43 -0300

    Model e Migrations Company no Laravel

---
+++ database/migrations/2020_06_04_134115_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('domain')->unique();
+            $table->string('bd_database')->unique();
+            $table->string('bd_hostname');
+            $table->string('bd_username');
+            $table->string('bd_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
