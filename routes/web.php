f915ca72cebcea0dd2a0237074f207fbabb08263
Amilton Soares <app@amiltonsoares.com.br>
2020-09-04 07:33:23 -0300

    Security update

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::view('/404-tenant', 'errors.404-tenant')->name('404.tenant');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
